875075b55344369096276dbebd5d619d4e8d1279
alexandrecesarba <alexandrecesarba@icloud.com>
2024-04-19 00:13:46 -0300

    added pointers to matrix mult

--- matrix_lib.c
+++ matrix_lib.c
@@ -34,29 +34,35 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix) {
     return 1;
 }
 
-int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC) {
-  if (matrixA->width != matrixB->height || matrixA->height != matrixC->height || matrixB->width != matrixC->width) return 0;
+int matrix_matrix_mult(struct matrix *a, struct matrix *b, struct matrix *c) {
+    if (a->width != b->height || a->height != c->height || b->width != c->width) return 0;
 
-  for (unsigned long int i = 0; i < matrixA->height; i++) {
-    for (unsigned long int j = 0; j < matrixB->width; j++) {
-      __m256 v_sum = _mm256_setzero_ps();
+    float *a_ptr, *b_ptr, *c_ptr;
 
-      for (unsigned long int k = 0; k < matrixA->width; k += 8) {
-        __m256 v_a = _mm256_loadu_ps(&matrixA->rows[i * matrixA->width + k]);
-        __m256 v_b = _mm256_loadu_ps(&matrixB->rows[k * matrixB->width + j]);
-      
-        v_sum = _mm256_fmadd_ps(v_a, v_b, v_sum);
-      }
+    for (unsigned long int i = 0; i < a->height; i++) {
+        for (unsigned long int j = 0; j < b->width; j++) {
+            __m256 v_sum = _mm256_setzero_ps();
 
-      v_sum = _mm256_hadd_ps(v_sum, v_sum);
-      v_sum = _mm256_hadd_ps(v_sum, v_sum);
+            a_ptr = &a->rows[i * a->width]; 
+            c_ptr = &c->rows[i * c->width + j]; 
 
-      float result[8];
-      _mm256_storeu_ps(result, v_sum);
-      
-      matrixC->rows[i * matrixB->width + j] = result[0] + result[4];
+            for (unsigned long int k = 0; k < a->width; k += 8) {
+                __m256 v_a = _mm256_loadu_ps(a_ptr);
+                b_ptr = &b->rows[k * b->width + j]; 
+                __m256 v_b = _mm256_loadu_ps(b_ptr);
+
+                v_sum = _mm256_fmadd_ps(v_a, v_b, v_sum);
+                a_ptr += 8; 
+            }
+
+            v_sum = _mm256_hadd_ps(v_sum, v_sum);
+            v_sum = _mm256_hadd_ps(v_sum, v_sum);
+
+            float result[8];
+            _mm256_storeu_ps(result, v_sum);
+            *c_ptr = result[0] + result[4]; 
+        }
     }
-  }
 
-  return 1;
+    return 1;
 }
